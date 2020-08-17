2d2e2029ce77a380c457a609c52d338c1108ae4f
Hugo Roca <hugo.rock20@hotmail.com>
2020-08-17 18:23:13 -0500

    add components and intall react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+
+import Navigation from "./components/Navigation";
+import CreateNote from "./components/CreateNote";
+import CreateUser from "./components/CreateUser";
+import NotesList from "./components/NotesList";
 
 function App() {
   return (
-    <div>
-      Hi!
-    </div>
+    <Router>
+      <Navigation />
+
+      <Route path="/" component={NotesList} exact />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
